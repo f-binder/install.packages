93cc3550f0690f60d6f163fc0c5961acf7d63f01
f-binder <53177048+f-binder@users.noreply.github.com>
2019-10-11 08:32:48 -0300

    Update install.new.R

--- install.new.R
+++ install.new.R
@@ -7,8 +7,8 @@ install.lib <- want.packages[!want.packages %in% installed.packages()[,"Package"
 
 # Installing subset of non-installed packages using a loop:
 for(lib in install.lib) {
-install.packages(lib,dependencies=TRUE)
+install.packages(lib, dependencies=TRUE)
 }
 
 # Requiere function (load all wanted packages)
-sapply(load.lib, require, character=TRUE)
+sapply(want.packages, require, character=TRUE)
